63a2ff3fbf1d1f5e840bbb52647cdd5975478a97
Kevin Hinshaw <kevin@hinshawfam.com>
2023-08-10 07:41:09 -0500

    listing files names added

---
+++ foreach-object.ps1
@@ -0,0 +1,39 @@
+﻿# $filepath = 'C:\Users\Kevin\OneDrive'
+
+# Get-ChildItem $filepath -File | ForEach-Object -MemberName {String}
+
+#ForEach (item in collection) {
+    # code to execute on each item
+#}
+
+#Read more: https://www.sharepointdiary.com/2020/07/powershell-foreach-foreach-object-guide.html#ixzz89zF3ZZVL
+
+
+#Object
+$Employee = [pscustomobject] @{
+    Name = "Shan Mathew"
+    Designation = "IT Manager"
+    Country = "United States"
+}
+
+#Get All Properties of the Object
+Foreach ($Property in $Employee.PSObject.Properties)
+{
+    Write-Host "$($Property.Name): $($Property.Value)"
+}
+
+#Read more: https://www.sharepointdiary.com/2020/07/powershell-foreach-foreach-object-guide.html#ixzz89zGa45w9
+
+#Get All Files from a Folder
+$Files = Get-ChildItem "C:\Users\Kevin\OneDrive\Documents\" | Where { !$_.PSIsContainer }
+ 
+#Print Each File Name in the Console
+ForEach ($File in $Files) {
+    Write-host $File.Name
+}
+
+
+
+
+
+
